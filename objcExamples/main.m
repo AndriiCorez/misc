9a8d0f160a43abd8fb77cb9cc25561ad1daf4ba4
Andres <andres@Andress-Mac.local>
2016-01-07 00:02:37 -0800

    Initial Commit

---
+++ objcExamples/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objcExamples
+//
+//  Created by Andres on 1/7/16.
+//  Copyright (c) 2016 Andres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
